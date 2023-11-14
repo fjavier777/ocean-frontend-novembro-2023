64ffa777e9172b11f3710cb0a365c91f246007fd
fjavier777 <113363561+fjavier777@users.noreply.github.com>
2023-11-14 00:46:52 +0000

    Extração do conteúdo para o <Card />

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,10 @@ import Card from './components/Card/Card';
 function App() {
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <div className="tags">
-          <div className="tag">Teste 1</div>
-          <div className="tag">Teste 2</div>
-          <div className="tag">Teste 3</div>
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
-      <br />
       
+      <br />
       <Card />
-
+      <Card />
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,11 @@
 export default function Card() {
-    return<div>Car está funcionando!</div>
-}
+    return<div className="card">
+        <h2>Rick Sanchez</h2>
+        <div className="tags">
+          <div className="tag">Teste 1</div>
+          <div className="tag">Teste 2</div>
+          <div className="tag">Teste 3</div>
+        </div>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
+}src/components/Card/Card.jsx
