1a19c1de4463da8d5f69911d95b5b8b1dbb8bb5b
fjavier777 <113363561+fjavier777@users.noreply.github.com>
2023-11-14 00:02:41 +0000

    Criação do componente Card (em progresso)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
+import Card from './components/Card/Card';
 
 function App() {
   return (
     <>
       <div className="card">
         <h2>Rick Sanchez</h2>
+        <div className="tags">
+          <div className="tag">Teste 1</div>
+          <div className="tag">Teste 2</div>
+          <div className="tag">Teste 3</div>
+        </div>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
       <br />
+      
+      <Card />
+
     </>
   )
 }
